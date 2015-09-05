005f3f872b7da822ca604813ae2cc095815c8a74
Chase Southard <chase.southard@gmail.com>
2015-09-05 18:34:54 -0400

    remove lint from build gulp action

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -144,7 +144,13 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('deploy', ['build'], ()=> {
+  return gulp.src('dist')
+    .pipe($.subtree())
+    .pipe($.clean());
+});
+
+gulp.task('build', ['html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
