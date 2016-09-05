9d5ab45e6ae966d165fe7014f34582cd7fc9ae19
Chase Southard <chase.southard@gmail.com>
2016-09-05 14:15:40 -0400

    holidays now uses xhr junk

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,12 +17,28 @@ $(function() {
     } else {
       // check if within normal working hours
       if (_.inRange(now.hour(), 11, 19)) {
-        if (now.holiday()) {
-          var holidays = now.holidays();
-          return setOutput('MAYBE.<br>It might be <br>' + holidays + '.');
-        } else {
-          return setOutput('YES');
-        }
+        now.holiday(
+          function(result){
+            console.log(result); // true
+            if (result == true) {
+              now.holidays(
+                function(result){
+                  console.log(result); // ["Christmas"]
+                  var holidays = result[0];
+                  return setOutput('MAYBE.<br>It might be <br>' + holidays + '.');
+                },
+                function(error){
+                  console.log(error); // IO error, JSON parse error...
+                }
+              );
+            } else {
+              return setOutput('YES');
+            }
+          },
+          function(error){
+            console.log(error); // IO error, JSON parse error...
+          }
+        );
       } else {
         return setOutput('NO');
       }
